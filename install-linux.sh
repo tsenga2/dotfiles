332081a825db6359a1c813b02a9677e81aa4b6cf
tsenga2 <tatsuro.senga@gmail.com>
2023-08-09 22:07:54 +0900

    install linux added initial
    Q
    q
    q
    D
    C
    xit
    exit
    quit
    quit

---
+++ install-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+sudo apt update
+sudo apt upgrade
+
+wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+
+sudo apt install intel-basekit
+sudo apt install intel-hpckit
+
+sudo apt install vim
+sudo apt install build-essential
+sudo apt install gfortran
+
+sudo apt install wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb

c0dca4040a3aae7ebdd7537028751e8b5a8d1f7b
tsenga2 <tatsuro.senga@gmail.com>
2023-08-09 22:16:03 +0900

    update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -8,9 +8,25 @@ echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt
 sudo apt install intel-basekit
 sudo apt install intel-hpckit
 
+sudo apt install git
 sudo apt install vim
 sudo apt install build-essential
 sudo apt install gfortran
+
+sudo apt install automake
+sudo apt install autoconf
+
+tar xvzf hdf5-1.14.1-2.tar.gz
+cd hdf5
+autoreconf -if
+./configure CC=icc FC=ifort --with-szlib --enable-fortran --enable-build-mode=production --prefix=$HOME/hdf5
+make -j
+make install
+
+git clone https://github.research.its.qmul.ac.uk/tew344/libsef-ts.git
+cd libsef-ts
+autoreconf -if
+./configure CC=icc FC=ifort --prefix=$HOME/install
 
 sudo apt install wget
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb

7c514cefa1c67e9a88ee2fd8391bbeb79bb0d7a0
tsenga2 <tatsuro.senga@gmail.com>
2024-03-26 10:42:12 +0900

    symbolic link sh added

---
+++ dotfiles-link.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zsh ~/.zshrc
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
